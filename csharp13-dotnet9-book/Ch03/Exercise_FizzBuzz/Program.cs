bd071d6bc5e34d19756721e8626f63ccadb81b23
yumerov <yumerov.levent@gmail.com>
2025-05-10 19:32:53 +0300

    [ch03] fizz buzz - for loop and if else

---
+++ csharp13-dotnet9-book/Ch03/Exercise_FizzBuzz/Program.cs
@@ -0,0 +1,37 @@
+﻿using static System.Console;
+
+const int fizzDenominator = 3;
+const int buzzDenominator = 5;
+const int total = 100;
+const int perLine = 10;
+
+for (int index = 1; index <= total; index++)
+{
+    bool alreadyFizzBuzz = false;
+    if (index % fizzDenominator == 0)
+    {
+        alreadyFizzBuzz = true;
+        Write("Fizz");
+    }
+
+    if (index % buzzDenominator == 0)
+    {
+        alreadyFizzBuzz = true;
+        Write("Buzz");
+    }
+    
+    if (!alreadyFizzBuzz)
+    {
+        Write(index);
+    }
+
+    if (index != total)
+    {
+        Write(", ");
+    }
+
+    if (index % perLine == 0)
+    {
+        WriteLine();
+    }
+}
