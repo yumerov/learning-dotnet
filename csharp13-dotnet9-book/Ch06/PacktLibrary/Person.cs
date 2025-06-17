6a5b067354d86dd22f2a8173c1d97d522693faa2
yumerov <yumerov.levent@gmail.com>
2025-06-17 23:15:20 +0300

    [ch06] OOP - instance and static

---
+++ csharp13-dotnet9-book/Ch06/PacktLibrary/Person.cs
@@ -0,0 +1,127 @@
+namespace Packt.Shared;
+
+public class Person
+{
+    # region Properties
+    public string? Name { get; set; }
+    public DateTimeOffset Born { get; set; }
+    public List<Person> Children { get; set; } = [];
+    // Allow multiple spouses to be stored for a person.
+    public List<Person> Spouses { get; set; } = [];
+    // A read-only property to show if a person is married to anyone.
+    public bool Married => Spouses.Count > 0;
+
+    # endregion
+    
+    # region Methods
+    # region Instance
+    public void WriteToConsole()
+    {
+        WriteLine($"{Name} was born on a {Born:dddd}.");
+    }
+    
+    public void WriteChildrenToConsole()
+    {
+        var term = Children.Count == 1 ? "child" : "children";
+        WriteLine($"{Name} has {Children.Count} {term}.");
+    }
+    
+    public Person ProcreateWith(Person partner)
+    {
+        return Procreate(this, partner);
+    }
+    
+    public void Marry(Person partner)
+    {
+        Marry(this, partner); 
+    }
+    
+    public void OutputSpouses()
+    {
+        if (Married)
+        {
+            string term = Spouses.Count == 1 ? "person" : "people";
+            WriteLine($"{Name} is married to {Spouses.Count} {term}:");
+            foreach (Person spouse in Spouses)
+            {
+                WriteLine($"  {spouse.Name}");
+            }
+        }
+        else
+        {
+            WriteLine($"{Name} is a singleton.");
+        }
+    }
+    # endregion
+    
+    # region Static
+    public static void Marry(Person p1, Person p2)
+    {
+        ArgumentNullException.ThrowIfNull(p1);
+        ArgumentNullException.ThrowIfNull(p2);
+        if (p1.Spouses.Contains(p2) || p2.Spouses.Contains(p1))
+        {
+            throw new ArgumentException(
+                string.Format("{0} is already married to {1}.",
+                    arg0: p1.Name, arg1: p2.Name));
+        }
+        p1.Spouses.Add(p2);
+        p2.Spouses.Add(p1);
+    }
+
+    /// <summary>
+    /// Static method to "multiply" aka procreate and have a child together.
+    /// </summary>
+    /// <param name="p1">Parent 1</param>
+    /// <param name="p2">Parent 2</param>
+    /// <returns>A Person object that is the child of Parent 1 and Parent 2.</returns>
+    /// <exception cref="ArgumentNullException">If p1 or p2 are null.</exception>
+    /// <exception cref="ArgumentException">If p1 and p2 are not married.</exception>
+    public static Person Procreate(Person p1, Person p2)
+    {
+        ArgumentNullException.ThrowIfNull(p1);
+        ArgumentNullException.ThrowIfNull(p2);
+        if (!p1.Spouses.Contains(p2) && !p2.Spouses.Contains(p1))
+        {
+            throw new ArgumentException(string.Format(
+                "{0} must be married to {1} to procreate with them.",
+                arg0: p1.Name, arg1: p2.Name));
+        }
+
+        Person baby = new()
+        {
+            Name = $"Baby of {p1.Name} and {p2.Name}",
+            Born = DateTimeOffset.Now
+        };
+        p1.Children.Add(baby);
+        p2.Children.Add(baby);
+        return baby;
+    }
+    # endregion
+    # endregion
+    
+    #region Operators
+    /// <summary>
+    /// Marry
+    /// </summary>
+    /// <see cref="Marry(Packt.Shared.Person)"/>
+    /// <param name="p1"></param>
+    /// <param name="p2"></param>
+    /// <returns></returns>
+    public static bool operator +(Person p1, Person p2)
+    {
+        Marry(p1, p2);
+
+        return p1.Married && p2.Married;
+    }
+    
+    /// <summary>
+    /// Procreate
+    /// </summary>
+    /// <see cref="Procreate"/>
+    /// <param name="p1"></param>
+    /// <param name="p2"></param>
+    /// <returns></returns>
+    public static Person operator *(Person p1, Person p2) => Procreate(p1, p2);
+    #endregion
+}
---
+++ csharp13-dotnet9-book/Ch06/PeopleApp/Program.cs
@@ -0,0 +1,62 @@
+﻿// See https://aka.ms/new-console-template for more information
+
+using Packt.Shared;
+
+Person harry = new()
+{
+    Name = "Harry",
+    Born = new(year: 2001, month: 3, day: 25,
+        hour: 0, minute: 0, second: 0,
+        offset: TimeSpan.Zero)
+};
+harry.WriteToConsole();
+
+// Statics
+Person lamech = new() { Name = "Lamech" };
+Person adah = new() { Name = "Adah" };
+Person zillah = new() { Name = "Zillah" };
+
+lamech.Marry(adah);
+Person.Marry(lamech, zillah);
+lamech.OutputSpouses();
+adah.OutputSpouses();
+zillah.OutputSpouses();
+
+var baby1 = lamech.ProcreateWith(adah);
+baby1.Name = "Jabal";
+WriteLine($"{baby1.Name} was born on {baby1.Born}");
+
+
+var baby2 = Person.Procreate(zillah, lamech);
+baby2.Name = "Tubalcain";
+adah.WriteChildrenToConsole();
+zillah.WriteChildrenToConsole();
+lamech.WriteChildrenToConsole();
+for (int i = 0; i < lamech.Children.Count; i++)
+{
+    WriteLine(format: "  {0}'s child #{1} is named \"{2}\".",
+        arg0: lamech.Name, arg1: i,
+        arg2: lamech.Children[i].Name);
+}
+
+
+try
+{
+    if (lamech + zillah)
+    {
+        WriteLine($"{lamech.Name} and {zillah.Name} successfully got married.");
+    }
+}
+catch (Exception e)
+{
+    WriteLine($"Error: {e.Message}");
+}
+
+
+var baby3 = lamech * adah;
+baby3.Name = "Jubal";
+var baby4 = zillah * lamech;
+baby4.Name = "Naamah";
+lamech.WriteChildrenToConsole();
+
+
