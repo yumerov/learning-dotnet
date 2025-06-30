4389d1b26524f137de76484c85b720746813ec9e
yumerov <yumerov.levent@gmail.com>
2025-06-30 22:38:06 +0300

    [c#13.net9][ch08] Working with big and complex numbers

---
+++ csharp13-dotnet9-book/ch08/WorkingWithNumbers/Program.cs
@@ -0,0 +1,31 @@
+﻿using System.Numerics;
+using static System.Console;
+
+const int width = 40;
+WriteLine("ulong.MaxValue vs a 30-digit BigInteger");
+WriteLine(new string('-', width));
+ulong big = ulong.MaxValue;
+WriteLine($"{big,width:N0}");
+BigInteger bigger = BigInteger.Parse("123456789012345678901234567890");
+WriteLine($"{bigger,width:N0}");
+
+WriteLine("Multiplying big integers");
+int number1 = 2_000_000_000;
+int number2 = 2;
+WriteLine($"number1: {number1:N0}");
+WriteLine($"number2: {number2:N0}");
+WriteLine($"number1 * number2: {number1 * number2:N0}");
+WriteLine($"Math.BigMul(number1, number2): {
+    Math.BigMul(number1, number2):N0}");
+WriteLine($"int.BigMul(number1, number2): {
+    int.BigMul(number1, number2):N0}");
+
+Complex c1 = new(real: 4, imaginary: 2);
+Complex c2 = new(real: 3, imaginary: 7);
+Complex c3 = c1 + c2;
+
+WriteLine($"{c1} added to {c2} is {c3}");
+WriteLine("{0} + {1}i added to {2} + {3}i is {4} + {5}i",
+    c1.Real, c1.Imaginary,
+    c2.Real, c2.Imaginary,
+    c3.Real, c3.Imaginary);
