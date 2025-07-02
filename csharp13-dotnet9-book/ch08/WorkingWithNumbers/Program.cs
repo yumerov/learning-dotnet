96082a8f3b10232c1df06052976a96c1513678d9
yumerov <yumerov.levent@gmail.com>
2025-07-02 21:13:09 +0300

    [c#13.net9][ch08] Common types - random

--- csharp13-dotnet9-book/ch08/WorkingWithNumbers/Program.cs
+++ csharp13-dotnet9-book/ch08/WorkingWithNumbers/Program.cs
@@ -29,3 +29,35 @@ WriteLine("{0} + {1}i added to {2} + {3}i is {4} + {5}i",
     c1.Real, c1.Imaginary,
     c2.Real, c2.Imaginary,
     c3.Real, c3.Imaginary);
+
+Random r = Random.Shared;
+int dieRoll = r.Next(minValue: 1, maxValue: 7); // Returns 1 to 6.
+WriteLine($"Random die roll: {dieRoll}");
+double randomReal = r.NextDouble();
+WriteLine($"Random double: {randomReal}");
+byte[] arrayOfBytes = new byte[256];
+r.NextBytes(arrayOfBytes);
+Write("Random bytes: ");
+foreach (var t in arrayOfBytes)
+{
+    Write($"{t:X2} ");
+}
+WriteLine();
+
+string[] beatles = r.GetItems(
+    choices: new[] { "John", "Paul", "George", "Ringo" },
+    length: 10);
+Write("Random ten beatles:");
+foreach (string beatle in beatles)
+{
+    Write($" {beatle}");
+}
+WriteLine();
+r.Shuffle(beatles);
+Write("Shuffled beatles:");
+foreach (string beatle in beatles)
+{
+    Write($" {beatle}");
+}
+WriteLine();
+
