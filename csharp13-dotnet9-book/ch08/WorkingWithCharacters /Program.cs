67743420feadd43921e9cff2a807139b5a46afd4
yumerov <yumerov.levent@gmail.com>
2025-07-02 22:16:38 +0300

    [c#13.net9][ch08] Common types - working with chars

--- csharp13-dotnet9-book/ch08/WorkingWithCharacters /Program.cs
+++ csharp13-dotnet9-book/ch08/WorkingWithCharacters /Program.cs
@@ -1,3 +1,18 @@
-﻿// See https://aka.ms/new-console-template for more information
+﻿using System.Buffers;
+using static System.Console;
 
-Console.WriteLine("Hello, World!");
+string vowels = "AEIOUaeiou";
+// .NET 8 and later.
+SearchValues<char> vowelsSearchValues = SearchValues.Create(vowels);   
+ReadOnlySpan<char> text = "Fred";
+WriteLine($"vowels: {vowels}");
+WriteLine($"text: {text}");
+WriteLine($"text.IndexOfAny(vowelsSearchValues): {text.IndexOfAny(vowelsSearchValues)}");
+
+string[] names = [ "Cassian", "Luthen", "Mon Mothma", "Dedra", "Syril", "Kino"];
+// .NET 9 and later.
+SearchValues<string> namesSearchValues = SearchValues.Create(names, StringComparison.OrdinalIgnoreCase);
+ReadOnlySpan<char> sentence = "In Andor, Diego Luna returns as the titular character, Cassian Andor, to whom audiences were first introduced in Rogue One.";
+WriteLine($"names: {string.Join(' ', names)}");
+WriteLine($"sentence: {sentence}");
+WriteLine($"sentence.IndexOfAny(vowelsSearchValues): {sentence.IndexOfAny(namesSearchValues)}");
