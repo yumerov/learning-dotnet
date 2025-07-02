08c2193045e953af807cec35a219a8716adc903f
yumerov <yumerov.levent@gmail.com>
2025-07-02 23:02:10 +0300

    [c#13.net9][ch08] Common types - regex

---
+++ csharp13-dotnet9-book/ch08/WorkingWithRegularExpressions/Program.cs
@@ -0,0 +1,7 @@
+﻿using System.Text.RegularExpressions;
+using static System.Console;
+
+Write("Enter your age: ");
+string input = ReadLine()!;
+Regex ageChecker = new(@"^\d+$");
+WriteLine(ageChecker.IsMatch(input) ? "Thank you!" : $"This is not a valid age: {input}");
