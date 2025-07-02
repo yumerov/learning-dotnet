0f2990357722c56b709fde336d70bf316419acd8
yumerov <yumerov.levent@gmail.com>
2025-07-02 21:15:27 +0300

    [c#13.net9][ch08] Common types - guid

--- csharp13-dotnet9-book/ch08/WorkingWithNumbers/Program.cs
+++ csharp13-dotnet9-book/ch08/WorkingWithNumbers/Program.cs
@@ -61,3 +61,19 @@ foreach (string beatle in beatles)
 }
 WriteLine();
 
+WriteLine($"Empty GUID: {Guid.Empty}.");
+Guid g = Guid.NewGuid();
+WriteLine($"Random GUID: {g}.");
+var guidAsBytes = g.ToByteArray();
+Write("GUID as byte array: ");
+foreach (var t in guidAsBytes)
+{
+    Write($"{t:X2} ");
+}
+WriteLine();
+WriteLine("Generating three v7 GUIDs:");
+for (int i = 0; i < 3; i++)
+{
+    Guid g7 = Guid.CreateVersion7(DateTimeOffset.UtcNow);
+    WriteLine($"  {g7}.");
+}
