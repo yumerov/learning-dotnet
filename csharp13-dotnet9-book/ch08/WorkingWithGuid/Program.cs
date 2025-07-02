86c6ef9a02f6671860ee5755647c42667f362b1b
yumerov <yumerov.levent@gmail.com>
2025-07-02 21:18:30 +0300

    [c#13.net9][ch08] Common types - moved to separated projects

---
+++ csharp13-dotnet9-book/ch08/WorkingWithGuid/Program.cs
@@ -0,0 +1,18 @@
+﻿using static System.Console;
+
+WriteLine($"Empty GUID: {Guid.Empty}.");
+Guid g = Guid.NewGuid();
+WriteLine($"Random GUID: {g}.");
+var guidAsBytes = g.ToByteArray();
+Write("GUID as byte array: ");
+foreach (var t in guidAsBytes)
+{
+    Write($"{t:X2} ");
+}
+WriteLine();
+WriteLine("Generating three v7 GUIDs:");
+for (int i = 0; i < 3; i++)
+{
+    Guid g7 = Guid.CreateVersion7(DateTimeOffset.UtcNow);
+    WriteLine($"  {g7}.");
+}
---
+++ csharp13-dotnet9-book/ch08/WorkingWithRandom/Program.cs
@@ -0,0 +1,32 @@
+﻿using static System.Console;
+
+Random r = Random.Shared;
+int dieRoll = r.Next(minValue: 1, maxValue: 7); // Returns 1 to 6.
+WriteLine($"Random die roll: {dieRoll}");
+double randomReal = r.NextDouble();
+WriteLine($"Random double: {randomReal}");
+byte[] arrayOfBytes = new byte[256];
+r.NextBytes(arrayOfBytes);
+Write("Random bytes: ");
+foreach (var t in arrayOfBytes)
+{
+    Write($"{t:X2} ");
+}
+WriteLine();
+
+string[] beatles = r.GetItems(
+    choices: new[] { "John", "Paul", "George", "Ringo" },
+    length: 10);
+Write("Random ten beatles:");
+foreach (string beatle in beatles)
+{
+    Write($" {beatle}");
+}
+WriteLine();
+r.Shuffle(beatles);
+Write("Shuffled beatles:");
+foreach (string beatle in beatles)
+{
+    Write($" {beatle}");
+}
+WriteLine();
