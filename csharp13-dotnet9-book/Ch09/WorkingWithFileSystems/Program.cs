68fa07b52d4b356585aac5d044c5f9afb510e3bf
yumerov <yumerov.levent@gmail.com>
2025-07-06 11:05:58 +0300

    [c#13.net9][ch09] managing directories and files

--- csharp13-dotnet9-book/Ch09/WorkingWithFileSystems/Program.cs
+++ csharp13-dotnet9-book/Ch09/WorkingWithFileSystems/Program.cs
@@ -48,3 +48,39 @@ foreach (DriveInfo drive in DriveInfo.GetDrives())
 }
 AnsiConsole.Write(drives);
 
+SectionTitle("Managing directories");
+string newFolder = Combine(GetFolderPath(SpecialFolder.Personal), Guid.NewGuid().ToString());
+Console.WriteLine($"Working with: {newFolder}");
+Console.WriteLine($"Does it exist? {Path.Exists(newFolder)}");
+Console.WriteLine("Creating it...");
+CreateDirectory(newFolder);
+
+Console.WriteLine($"Does it exist? {Directory.Exists(newFolder)}");
+Console.Write("Confirm the directory exists, and then press any key.");
+Console.ReadKey(intercept: true);
+Console.WriteLine("Deleting it...");
+Delete(newFolder, recursive: true);
+Console.WriteLine($"Does it exist? {Path.Exists(newFolder)}");
+
+SectionTitle("Managing files");
+string dir = Combine(GetFolderPath(SpecialFolder.Personal), "OutputFiles");
+CreateDirectory(dir);
+
+string textFile = Combine(dir, "Dummy.txt");
+string backupFile = Combine(dir, "Dummy.bak");
+Console.WriteLine($"Working with: {textFile}");
+Console.WriteLine($"Does it exist? {File.Exists(textFile)}");
+StreamWriter textWriter = File.CreateText(textFile);
+textWriter.WriteLine("Hello, C#!");
+textWriter.Close();
+Console.WriteLine($"Does it exist? {File.Exists(textFile)}");
+File.Copy(sourceFileName: textFile, destFileName: backupFile, overwrite: true);
+Console.WriteLine($"Does {backupFile} exist? {File.Exists(backupFile)}");
+Console.Write("Confirm the files exist, and then press any key.");
+Console.ReadKey(intercept: true);
+File.Delete(textFile);
+Console.WriteLine($"Does it exist? {File.Exists(textFile)}");
+Console.WriteLine($"Reading contents of {backupFile}:");
+StreamReader textReader = File.OpenText(backupFile);
+Console.WriteLine(textReader.ReadToEnd());
+textReader.Close();
