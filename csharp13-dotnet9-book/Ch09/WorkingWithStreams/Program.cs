5d106a90efc42bdcc14e2d045ab10c252311e5b2
yumerov <yumerov.levent@gmail.com>
2025-07-06 14:24:41 +0300

    [c#13.net9][ch09] working with text file and xml streams

---
+++ csharp13-dotnet9-book/Ch09/WorkingWithStreams/Program.cs
@@ -0,0 +1,50 @@
+﻿using System.Xml;
+using WorkingWithStreams;
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+SectionTitle("Writing to text streams");
+string textFile = Combine(CurrentDirectory, "streams.txt");
+StreamWriter text = File.CreateText(textFile);
+foreach (string item in Viper.Callsigns)
+{
+    text.WriteLine(item);
+}
+text.Close();
+OutputFileInfo(textFile);
+
+SectionTitle("Writing to XML streams");
+string xmlFile = Combine(CurrentDirectory, "streams.xml");
+FileStream? xmlFileStream = null;
+XmlWriter? xml = null;
+try
+{
+    xmlFileStream = File.Create(xmlFile);
+    xml = XmlWriter.Create(xmlFileStream, new XmlWriterSettings { Indent = true });
+    xml.WriteStartDocument();
+    xml.WriteStartElement("callsigns");
+    foreach (string item in Viper.Callsigns)
+    {
+        xml.WriteElementString("callsign", item);
+    }
+    xml.WriteEndElement();
+}
+catch (Exception ex)
+{
+    WriteLine($"{ex.GetType()} says {ex.Message}");
+}
+finally
+{
+    if (xml is not null)
+    {
+        xml.Close();
+        WriteLine("The XML writer's unmanaged resources have been disposed.");
+    }
+    if (xmlFileStream is not null)
+    {
+        xmlFileStream.Close();
+        WriteLine("The file stream's unmanaged resources have been disposed.");
+    }
+}
+OutputFileInfo(xmlFile);
---
+++ csharp13-dotnet9-book/Ch09/WorkingWithStreams/Viper.cs
@@ -0,0 +1,10 @@
+namespace WorkingWithStreams;
+
+public class Viper
+{
+    public static string[] Callsigns =
+    [
+        "Husker", "Starbuck", "Apollo", "Boomer",
+        "Bulldog", "Athena", "Helo", "Racetrack"
+    ];
+}
