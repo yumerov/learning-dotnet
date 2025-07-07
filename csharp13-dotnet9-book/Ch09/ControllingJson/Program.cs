7f670b6f56efac1ba6971d5ee7cf2df4b1e69aea
yumerov <yumerov.levent@gmail.com>
2025-07-07 21:13:25 +0300

    [c#13.net9][ch09] native json

---
+++ csharp13-dotnet9-book/Ch09/ControllingJson/Program.cs
@@ -0,0 +1,34 @@
+﻿using System.Text.Json;
+using ControllingJson;
+using static System.Console;
+using static System.Environment;
+using static System.IO.Path;
+
+Book csharpBook = new(title:
+    "C# 13 and .NET 9 - Modern Cross-Platform Development Fundamentals")
+{
+    Author = "Mark J Price",
+    PublishDate = new DateTime(year: 2024, month: 11, day: 12),
+    Pages = 823,
+    Created = DateTimeOffset.UtcNow,
+};
+JsonSerializerOptions options = new()
+{
+    IncludeFields = true,
+    PropertyNameCaseInsensitive = true,
+    WriteIndented = true,
+    PropertyNamingPolicy = JsonNamingPolicy.CamelCase,
+};
+var path = Combine(CurrentDirectory, "book.json");
+using (Stream fileStream = File.Create(path))
+{
+    JsonSerializer.Serialize(utf8Json: fileStream, value: csharpBook, options);
+}
+
+WriteLine("**** File Info ****");
+WriteLine($"File: {GetFileName(path)}");
+WriteLine($"Path: {GetDirectoryName(path)}");
+WriteLine($"Size: {new FileInfo(path).Length:N0} bytes.");
+WriteLine("/------------------");
+WriteLine(File.ReadAllText(path));
+WriteLine("------------------/");
